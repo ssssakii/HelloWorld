249a0cfcf7563085bd94f3095d5aa65baf01cb2e
g15948ts <g15948ts@s30420.ACTSUDA>
2017-04-14 11:28:12 +0900

    firstcommit

--- src/firsthelloworld/FirstHelloWorld.java
+++ src/firsthelloworld/FirstHelloWorld.java
@@ -18,7 +18,7 @@ public class FirstHelloWorld {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        println("HelloWorld");
+        println("HelloWorldです");
     }
     
 }
